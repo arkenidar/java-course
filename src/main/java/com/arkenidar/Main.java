08f9e744b6b484be98a5368ce0a8433081dea5cd
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-09-02 14:28:30 +0200

    min and max excercise

--- src/main/java/com/arkenidar/Main.java
+++ src/main/java/com/arkenidar/Main.java
@@ -2,6 +2,26 @@ package com.arkenidar;
 
 public class Main {
     public static void main(String[] args) {
+        minAndMaxTesting();
+    }
+
+    public static void minAndMaxTesting(){
+        String line="3,2,1,-1";
+        
+        String[] split = line.split(",");
+
+        Integer min=null, max=null;
+        for(String is: split){
+            int i=Integer.parseInt(is);
+            if(min==null) min=i;
+            if(max==null) max=i;
+            if(i<min) min=i;
+            if(i>max) max=i;
+        }
+        System.out.printf("min: %d ; max: %d",min,max);
+    }
+    
+    public static void anagramTesting() {
         String pairs[][] = new String[][]{
                 {"aba", "baa"},
                 {"ab", "aa"},
